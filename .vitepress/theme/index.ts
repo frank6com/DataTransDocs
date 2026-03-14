0dc573babfae5c1595e9418973e5bad5dbed7cda
Frank6 <liuchunbo@qq.com>
2026-03-14 16:43:33 +0800

    修正giscus配置

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,19 +1,23 @@
-// https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import GiscusComment from '../components/GiscusComment.vue'
-import './style.css'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus'
+import { useData, useRoute } from 'vitepress'
 
 export default {
-  extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'page-bottom': () => h(GiscusComment)
+  ...DefaultTheme,
+  setup() {
+    const { frontmatter } = useData()
+    const route = useRoute()
+
+    giscusTalk({
+      repo: 'Frank6com',
+      repoId: 'R_kgDORdSF5g',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDORdSF5s4C36lq',
+      mapping: 'pathname',
+      theme: 'preferred_color_scheme'
+    }, {
+      frontmatter,
+      route
     })
-  },
-  enhanceApp({ app, router, siteData }) {
-    // ...
   }
-} satisfies Theme
+}
