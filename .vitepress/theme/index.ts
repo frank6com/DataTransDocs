441de4e6e9234889605f931b53dafaadcf94d318
Frank6 <liuchunbo@qq.com>
2026-03-17 21:10:11 +0800

    修正photoswipe配置错误

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
 import giscusTalk from 'vitepress-plugin-comment-with-giscus'
 import { useData, useRoute } from 'vitepress'
-import { onMounted } from 'vue'
+import { onMounted, watch, nextTick } from 'vue'
 import strict from 'node:assert/strict'
 import './style.css'
 import PhotoSwipeLightbox from 'photoswipe/lightbox'
@@ -34,28 +34,33 @@ export default {
       route
     })
     
-    onMounted(() => {
-      const lightbox = new PhotoSwipeLightbox({
-        gallery: '.main',
-        children: 'img:not(.logo):not(.no-zoom)',
-        pswpModule: PhotoSwipe,
-        padding: { top: 20, bottom: 20, left: 20, right: 20 },
-        wheelToZoom: true,
-        showHideAnimationType: 'zoom'
-      })
-      
-      lightbox.on('itemData', (e) => {
-        const itemData = e.itemData
-        const element = itemData.element as HTMLImageElement
+    const initPhotoSwipe = () => {
+      nextTick(() => {
+        const lightbox = new PhotoSwipeLightbox({
+          gallery: '.main',
+          children: 'img:not(.logo):not(.no-zoom)',
+          pswpModule: PhotoSwipe,
+          padding: { top: 20, bottom: 20, left: 20, right: 20 },
+          wheelToZoom: true,
+          showHideAnimationType: 'zoom'
+        })
         
-        if (element.tagName === 'IMG') {
-          itemData.src = element.src
-          itemData.w = element.naturalWidth
-          itemData.h = element.naturalHeight
-        }
+        lightbox.on('itemData', (e) => {
+          const itemData = e.itemData
+          const element = itemData.element as HTMLImageElement
+          
+          if (element.tagName === 'IMG') {
+            itemData.src = element.src
+            itemData.w = element.naturalWidth
+            itemData.h = element.naturalHeight
+          }
+        })
+        
+        lightbox.init()
       })
-      
-      lightbox.init()
-    })
+    }
+    
+    onMounted(initPhotoSwipe)
+    watch(() => route.path, initPhotoSwipe)
   }
 }
