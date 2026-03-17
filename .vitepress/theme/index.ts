73177417a8d3d514267f82530cd49a2d53ae214a
Frank6 <liuchunbo@qq.com>
2026-03-17 14:48:16 +0800

    增加photoswipe预览支持

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,12 @@
 import DefaultTheme from 'vitepress/theme'
 import giscusTalk from 'vitepress-plugin-comment-with-giscus'
 import { useData, useRoute } from 'vitepress'
+import { onMounted } from 'vue'
 import strict from 'node:assert/strict'
 import './style.css'
+import PhotoSwipeLightbox from 'photoswipe/lightbox'
+import PhotoSwipe from 'photoswipe'
+import 'photoswipe/style.css'
 
 export default {
   ...DefaultTheme,
@@ -28,6 +32,30 @@ export default {
     }, {
       frontmatter,
       route
+    })
+    
+    onMounted(() => {
+      const lightbox = new PhotoSwipeLightbox({
+        gallery: '.main',
+        children: 'img:not(.logo):not(.no-zoom)',
+        pswpModule: PhotoSwipe,
+        padding: { top: 20, bottom: 20, left: 20, right: 20 },
+        wheelToZoom: true,
+        showHideAnimationType: 'zoom'
+      })
+      
+      lightbox.on('itemData', (e) => {
+        const itemData = e.itemData
+        const element = itemData.element as HTMLImageElement
+        
+        if (element.tagName === 'IMG') {
+          itemData.src = element.src
+          itemData.w = element.naturalWidth
+          itemData.h = element.naturalHeight
+        }
+      })
+      
+      lightbox.init()
     })
   }
 }
